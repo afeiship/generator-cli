8ddff1e31442008b6bb4be3816d5f6fc03bf4102
afeiship <1290657123@qq.com>
2021-03-06 15:00:29 +0800

    feat: add registy

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,50 +10,19 @@ const replace = require('replace-in-file');
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
     this.log(yosay(`Welcome to the stunning ${chalk.red('generator-cli')} generator!`));
-
-    const prompts = [
-      {
-        type: 'input',
-        name: 'scope',
-        message: 'Your scope (eg: `babel`)?',
-        default: 'jswork'
-      },
-      {
-        type: 'input',
-        name: 'project_name',
-        message: 'Your project_name (eg: like this `my-cli` )?',
-        default: yoHelper.discoverRoot
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Your description?',
-        validate: Boolean
-      }
-    ];
-
-    return this.prompt(prompts).then(
-      function (props) {
-        // To access props later use this.props.someAnswer;
-        this.props = props;
-        yoHelper.rewriteProps(props);
-      }.bind(this)
-    );
+    return this.prompt(prompts).then((props) => {
+      this.props = props;
+      yoHelper.rewriteProps(props);
+    });
   }
 
   writing() {
     const done = this.async();
-    remote(
-      'afeiship',
-      'boilerplate-cli',
-      function (err, cachePath) {
-        // copy files:
-        this.fs.copy(glob.sync(resolve(cachePath, '{**,.*}')), this.destinationPath());
-        done();
-      }.bind(this)
-    );
+    remote('afeiship', 'boilerplate-cli', (err, cachePath) => {
+      this.fs.copyTpl(glob.sync(resolve(cachePath, '{**,.*}')), this.destinationPath(), this.props);
+      done();
+    });
   }
 
   end() {
@@ -62,16 +31,8 @@ module.exports = class extends Generator {
 
     replace.sync({
       files,
-      from: [
-        /boilerplate-scope/g,
-        /boilerplate-cli-description/g,
-        /boilerplate-cli/g
-      ],
-      to: [
-        scope,
-        description,
-        project_name
-      ]
+      from: [/boilerplate-scope/g, /boilerplate-cli-description/g, /boilerplate-cli/g],
+      to: [scope, description, project_name]
     });
   }
 };
