e70a51e95d0f62a996d85ca3f1aceca1e1569b82
afeiship <1290657123@qq.com>
2021-03-06 15:07:23 +0800

    fix: prompts import

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,6 +7,7 @@ const { resolve } = require('path');
 const remote = require('yeoman-remote');
 const yoHelper = require('@jswork/yeoman-generator-helper');
 const replace = require('replace-in-file');
+const prompts = require('./prompts');
 
 module.exports = class extends Generator {
   prompting() {
