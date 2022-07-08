d440a0bc917038006438bf0dd6eb3f54e4579e4a
afeiship <1290657123@qq.com>
2022-07-08 08:41:12 +0800

    chore: add single cli generator

--- generators/single/index.js
+++ generators/single/index.js
@@ -1,7 +1,16 @@
 'use strict';
 const Generator = require('@jswork/yeoman-generator');
+const getp = require('@jswork/generator-prompts');
+const yosay = require('yosay');
+const prompts = getp(['scope', 'registry', 'project_name', 'description']);
 
 module.exports = class extends Generator {
+  async prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(`Welcome to the stunning 'cli:single' generator!`));
+    this.props = await this.prompt(prompts);
+  }
+
   writing() {
     this.fs.copyTpl(this.srcFiles, this.destinationPath(), this.props);
   }
