4a2c6f70dc74ab83de232457a4410b83bd5b9819
afeiship <1290657123@qq.com>
2023-03-21 10:50:05 +0800

    refactor: nx use jsw-generator base

--- generators/nx/index.js
+++ generators/nx/index.js
@@ -1,26 +1,17 @@
 'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
 const yosay = require('yosay');
-const globby = require('globby');
-const yoHelper = require('@jswork/yeoman-generator-helper');
+const Generator = require('@jswork/yeoman-generator');
 const getp = require('@jswork/generator-prompts');
 const prompts = getp(['scope', 'registry', 'project_name', 'description']);
 
 module.exports = class extends Generator {
-  prompting() {
-    this.log(yosay(`Welcome to the stunning ${chalk.red('generator-cli')} generator!`));
-    return this.prompt(prompts).then((props) => {
-      this.props = props;
-      yoHelper.rewriteProps(props);
-    });
+  async prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(`Welcome to the stunning 'cli:nx' generator!`));
+    this.props = await this.prompt(prompts);
   }
 
   writing() {
-    this.fs.copyTpl(
-      globby.sync(this.templatePath('**'), { dot: true }),
-      this.destinationPath(),
-      this.props
-    );
+    this.fs.copyTpl(this.srcFiles, this.destinationPath(), this.props);
   }
 };
