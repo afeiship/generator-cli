6c26b6ad7f353a68d5d21b028916063e4f649530
afeiship <1290657123@qq.com>
2023-04-18 21:13:26 +0800

    feat: esm missing + cil tmpl

--- generators/single/templates/lib/index.js
+++ generators/single/templates/lib/index.js
@@ -1,3 +1,15 @@
-export function cli(args) {
-  console.log('hello args', args);
+import { Command } from 'commander';
+import { version } from '../package.json';
+
+const program = new Command();
+program.version(version);
+
+program
+  .option('-s, --src <string>', 'source filepath.', './src/myfile/sqlite3.db')
+  .option('-d, --dst <string>', 'destination filepath.', '/Websites/test/')
+  .parse(process.argv);
+
+export function cli() {
+  const opts = program.opts();
+  console.log('hello cli: ', opts);
 }
